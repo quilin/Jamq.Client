90b051606f09ced43c2b96fa17e8457fa86a799d
Vladislav Quilin <v.quilin@gmail.com>
2022-06-10 20:41:56 +0200

    [RMQ.Producer] Producer builder implementation

---
+++ src/RMQ.Client/Producing/ProducerBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RMQ.Client.Abstractions;
+using RMQ.Client.Abstractions.Producing;
+using RMQ.Client.Connection;
+
+namespace RMQ.Client.Producing;
+
+internal class ProducerBuilder : IProducerBuilder
+{
+    private readonly IList<Func<ProducerDelegate, ProducerDelegate>> components =
+        new List<Func<ProducerDelegate, ProducerDelegate>>();
+
+    public ProducerBuilder(
+        IServiceProvider serviceProvider)
+    {
+        ServiceProvider = serviceProvider;
+    }
+    
+    public IProducerBuilder With(Func<ProducerDelegate, ProducerDelegate> middleware)
+    {
+        components.Add(middleware);
+        return this;
+    }
+
+    public IProducerBuilder Flush()
+    {
+        components.Clear();
+        return this;
+    }
+
+    public IProducer BuildRabbit(RabbitProducerParameters parameters)
+    {
+        var channelPool = ServiceProvider.GetRequiredService<IProducerChannelPool>();
+        return new Producer(channelPool, ServiceProvider, parameters, components);
+    }
+
+    public IServiceProvider ServiceProvider { get; }
+}
