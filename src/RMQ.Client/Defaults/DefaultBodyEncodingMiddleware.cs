5b9a2c81b2d61262d7eca0af14fef1f80904d0f8
Vladislav Quilin <v.quilin@gmail.com>
2022-06-10 20:42:41 +0200

    [RMQ.Producer] Producer encoding middleware

---
+++ src/RMQ.Client/Defaults/DefaultBodyEncodingMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using RMQ.Client.Abstractions.Producing;
+
+namespace RMQ.Client.Defaults;
+
+internal class DefaultBodyEncodingMiddleware : IProducerMiddleware
+{
+    private static readonly JsonSerializerOptions SerializerOptions = new(JsonSerializerDefaults.Web);
+
+    public Task InvokeAsync(
+        ProducerContext context,
+        ProducerDelegate next)
+    {
+        var body = JsonSerializer.SerializeToUtf8Bytes(context.Message, SerializerOptions);
+        context.Body = body;
+        return next.Invoke(context);
+    }
+}
